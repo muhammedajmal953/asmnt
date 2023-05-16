807162107e2cb3a7da8611f85a3629bd2778ffd0
muhammed_ajmal <muhammedajmal953@gmail.com>
2023-05-16 13:01:38 +0530

    assinmnet2

---
+++ script.js
@@ -0,0 +1,29 @@
+let a=9
+for (let i = 1; i <=10; i++) {
+    console.log(i,"*",a,"=",i*a);
+    }
+
+    
+    
+let arr=[1,3,9,12,18,21]
+for (let i = 0; i < arr.length; i++) {
+    console.log("sqare of "+arr[i]+"="+arr[i]**2);
+    
+}
+let mark=70
+if (mark>90) {
+    console.log("your grade is A");
+}else if(mark>70)
+{
+    console.log("your grade is B");
+}else if(mark>50)
+{
+    console.log("your grade is C");
+}else if(mark>30)
+{
+    console.log("your grade is D");
+} 
+else{
+    console.log("your grade is e");
+}
+
